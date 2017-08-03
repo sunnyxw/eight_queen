772a8243c034df6390e6204376d1c21f89a8aff8
Xiaowei Yang <christianaweather@gmail.com>
2017-08-02 22:08:11 -0400

    why background color doesn't change

--- src/Chess/Chess.js
+++ src/Chess/Chess.js
@@ -6,12 +6,23 @@ class Chess extends Component {
   constructor(props){
     super(props);
     this.handleClick=this.handleClick.bind(this);
+    this.toggleChess=this.toggleChess.bind(this);
   }
   handleClick(e){
     var x=this.props.position.x;
     var y=this.props.position.y;
     e.preventDefault();
+    this.toggleChess();
     console.log(x, y);
+  }
+
+  toggleChess(){
+    var color = document.getElementById("Chess").background-color;
+    if (color == "white"){
+      color = "yellow";}
+    else{
+      color = "white";
+    }
   }
 
   render() {
@@ -20,7 +31,7 @@ class Chess extends Component {
 
     return (
         <div className="Chess col-xs-8 col-custom-eight" 
-        id={(x, y)} onClick={this.handleClick}>
+        id="Chess" onClick={this.handleClick}>
               {x}, {y}
         </div>
     );
